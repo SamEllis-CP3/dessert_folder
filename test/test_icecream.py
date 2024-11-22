c88461557fc5e49329f6f1bcb3f5f3746f1ca512
SamEllis-CP3 <samuel.ellis@ucas-edu.net>
2024-11-22 11:34:23 -0700

    adsadsadads

--- test/test_icecream.py
+++ test/test_icecream.py
@@ -8,11 +8,17 @@ def test_ice_cream():
     assert test_icecream_1.get_cost() == 2.50
     assert test_icecream_1.get_scoop() == 2
     assert test_icecream_1.get_name() == "Starwberry"
+    assert test_icecream_1.calc_cost() == 2.50
+    assert test_icecream_1.calculate_tax() == 0.18
 
     assert test_icecream_2.get_cost() == 1.75
     assert test_icecream_2.get_scoop() == 3
     assert test_icecream_2.get_name() == "Chocolate"
+    assert test_icecream_2.calc_cost() == 1.75
+    assert test_icecream_2.calculate_tax() == 0.13
 
     assert test_icecream_3.get_cost() == 3.50
     assert test_icecream_3.get_scoop() == 1
     assert test_icecream_3.get_name() == "Pecan"
+    assert test_icecream_3.calc_cost() == 3.50
+    assert test_icecream_3.calculate_tax() == 0.25
--- test/test_sundae.py
+++ test/test_sundae.py
@@ -10,15 +10,21 @@ def test_sundae():
     assert test_sun1.get_name() == "Starwberry"
     assert test_sun1.get_topping() == "sprinkles"
     assert test_sun1.get_topping_price() == 0.25
+    assert test_sun1.calc_cost() == 2.75
+    assert test_sun1.calculate_tax() == 0.18
 
     assert test_sun2.get_cost() == 1.75
     assert test_sun2.get_scoop() == 3
     assert test_sun2.get_name() == "Chocolate"
     assert test_sun2.get_topping() == "Fudge"
     assert test_sun2.get_topping_price() == 0.75
+    assert test_sun2.calc_cost() == 2.50
+    assert test_sun2.calculate_tax() == 0.13
 
     assert test_sun3.get_cost() == 3.50
     assert test_sun3.get_scoop() == 1
     assert test_sun3.get_name() == "Pecan"
     assert test_sun3.get_topping() == "Nuts"
     assert test_sun3.get_topping_price() == 0.50
+    assert test_sun3.calc_cost() == 4.00
+    assert test_sun3.calculate_tax() == 0.25
